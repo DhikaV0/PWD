793ced169f1c195ed80d128d84d5e8087304565c
DhikaV0 <judhapratamaa@gmial.com>
2024-09-13 17:56:44 +0700

    Updated home blade

---
+++ app/Models/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens; // Import trait HasApiTokens dari namespace yang benar
+use Illuminate\Database\Eloquent\Factories\HasFactory; // Pastikan juga HasFactory di-import dengan benar
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
---
+++ routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!php artisan key:generate
+|
+*/
+
+
+Route::get('/', function () {
+    return view('landpage');
+});
+
+// Route untuk halaman login
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+// Route untuk halaman home
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
+// Route untuk halaman register
+Route::get('/register', function () {
+    return view('register');
+})->name('register');
+
+// Route Register
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register.form');
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
+
+// Route untuk halaman login
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
+
+// Route untuk logout
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
